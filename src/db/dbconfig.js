063080713e880a3893c83a13677edb47b6cb714d
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2024-05-28 22:26:42 -0300

    Updated database path

--- src/db/dbconfig.js
+++ src/db/dbconfig.js
@@ -1,14 +1,21 @@
+/* eslint-disable no-undef */
 import knex from 'knex';
 import {fileURLToPath} from 'url';
 import {dirname, join} from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+import logger from '../loggerUtil/logger.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const databaseEnv = process.env.DATABASE_ENV;
+
+logger.info(`DATABASE ENVIRONMENT: ${databaseEnv}.`);
 
 const dbConfig = {
   client: 'sqlite3',
   connection: {
-    filename: join(__dirname, './glicocheck_db.sqlite'),
+    filename: join(__dirname, `./${databaseEnv}/glicocheck_db.sqlite`),
   },
   useNullAsDefault: true,
 };
