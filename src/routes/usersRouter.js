2feae507987e80d13380d8744aa2e81502e13a3e
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2024-05-30 22:40:22 -0300

    Added JWT token checking

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const usersRouter = express.Router();
 import UserController from '../controllers/userController.js';
+import SecurityUtils from '../utils/securityUtils.js';
 
 usersRouter.use(express.json());
+usersRouter.use(SecurityUtils.checkToken);
 
 usersRouter
   .get('/', UserController.getAllUsers)
