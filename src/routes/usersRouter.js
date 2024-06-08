a76942d4025672b8baf1fba7905870c3dd127e50
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2024-06-08 15:09:38 -0300

    Removed JWT token validation from add new user. (#18)

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,14 +5,29 @@ import AuthMiddleware from '../routes/middleware/authMiddleware.js';
 import RoleMiddleware from '../routes/middleware/roleMiddleware.js';
 import UserCodeMiddleware from '../routes/middleware/userCodeMiddleware.js';
 
-usersRouter.use(AuthMiddleware.checkToken);
 usersRouter.use(express.json());
 
 usersRouter
-  .get('/', RoleMiddleware.isAdminUser, UserController.getAllUsers)
+  .get('/',
+    AuthMiddleware.checkToken,
+    RoleMiddleware.isAdminUser,
+    UserController.getAllUsers
+  )
   .post('/', UserController.addUser)
-  .get('/:usercode', UserCodeMiddleware.validateUserCode, UserController.getUserByUserCode)
-  .put('/:usercode', UserCodeMiddleware.validateUserCode, UserController.updateUserByUserCode)
-  .delete('/:usercode', UserCodeMiddleware.validateUserCode, UserController.deleteUserByUserCode);
+  .get('/:usercode',
+    AuthMiddleware.checkToken,
+    UserCodeMiddleware.validateUserCode,
+    UserController.getUserByUserCode
+  )
+  .put('/:usercode',
+    AuthMiddleware.checkToken,
+    UserCodeMiddleware.validateUserCode,
+    UserController.updateUserByUserCode
+  )
+  .delete('/:usercode',
+    AuthMiddleware.checkToken,
+    UserCodeMiddleware.validateUserCode,
+    UserController.deleteUserByUserCode
+  );
 
 export default usersRouter;
