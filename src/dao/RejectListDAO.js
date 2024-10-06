8befb97439ae2661cc2870c26b4efd14e47aecfe
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2024-10-06 16:02:53 -0300

    Deleting old JWT token by timestamp.

--- src/dao/RejectListDAO.js
+++ src/dao/RejectListDAO.js
@@ -18,7 +18,7 @@ export default class RejectListDAO {
         return { success: false, message: Messages.ERROR };
       }
     } catch (error) {
-      logger.error('Error RejectListDAO.add');
+      logger.error(`Error RejectListDAO.add - Details: ${error}.`);
       throw new Error(Messages.ERROR);
     }
   }
@@ -35,7 +35,7 @@ export default class RejectListDAO {
         return { success: false, message: Messages.NOTHING_FOUND };
       }
     } catch (error) {
-      logger.error('Error RejectListDAO.getById');
+      logger.error(`Error RejectListDAO.getById - Details: ${error}.`);
       throw new Error(Messages.ERROR);
     }
   }
@@ -43,15 +43,16 @@ export default class RejectListDAO {
   static async deleteRegistersOlderThan(hours) {
     try {
       const hoursAgo = new Date(new Date().getTime() - hours * 3600000);
-      const formattedDate = this.formatDate(hoursAgo);
-
+      // const formattedDate = this.formatDate(hoursAgo);
+      const timestamp = hoursAgo.toISOString();
+    
       await database(TABLE_NAME)
-        .where('created_at', '<', formattedDate)
+        .where('created_at', '<', timestamp)
         .del();
 
       return { success: true };
     } catch (error) {
-      logger.error('Error RejectListDAO.deleteRegistersOlderThan - ', error);
+      logger.error(`Error RejectListDAO.deleteRegistersOlderThan - Details: ${error.errors}.`);
       return { success: false };
     }
   }
@@ -61,14 +62,12 @@ export default class RejectListDAO {
    * @param {Date} hour The hour to be formatted.
    * @returns A string with the date in the same format that is stored in the database.
    */
-  static async formatDate(hour) {
-    return `${hour.getFullYear()}-
-      ${(hour.getMonth() + 1).toString().padStart(2, '0')}-${hour
-        .getDate()
-        .toString()
-        .padStart(2, '0')} ${hour.getHours().toString()
-        .padStart(2, '0')}:${hour.getMinutes().toString()
-        .padStart(2, '0')}:${hour.getSeconds().toString()
-        .padStart(2, '0')}`;
+  static formatDate(hour) {
+    return `${hour.getFullYear()}-${(hour.getMonth() + 1).toString()
+        .padStart(2, '0')}-${hour.getDate().toString()}`;
+        // .padStart(2, '0')} ${hour.getHours().toString()
+        // .padStart(2, '0')}:${hour.getMinutes().toString()
+        // .padStart(2, '0')}:${hour.getSeconds().toString()
+        // .padStart(2, '0')}`;
   }
 }
