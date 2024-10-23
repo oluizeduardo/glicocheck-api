e2e52f3c0b069a18efd2a1ed6be576eb638d1c5e
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2024-10-23 12:29:30 -0300

    Updated Diabetes Type API response

--- src/controllers/diabetesTypeController.js
+++ src/controllers/diabetesTypeController.js
@@ -6,40 +6,23 @@ import DiabetesTypeDAO from '../dao/DiabetesTypeDAO.js';
  * DiabetesTypeController.
  */
 class DiabetesTypeController {
-  /**
-   * Retrieves all diabetes types.
-   *
-   * @async
-   * @param {Object} req - The request object.
-   * @param {Object} res - The response object.
-   * @return {Promise<void>} - A promise that resolves to void.
-   * @throws {Error} - If an error occurs during the process.
-   */
+
   static getAllTypes = async (req, res) => {
     logger.info('Executing DiabetesTypeController.getAllTypes');
     try {
       const result = await DiabetesTypeDAO.getAll();
 
       if (result.success) {
-        res.status(200).json(result.types);
+        return res.status(200).json(result.types);
       } else {
-        res.status(404).json({ message: result.message });
+        return res.status(404).json({ message: result.message });
       }
     } catch (error) {
-      logger.error('Error DiabetesTypeController.getAllTypes');
-      res.status(500).json({ message: Messages.ERROR });
+      logger.error(`Error DiabetesTypeController.getAllTypes - Details: ${error}`);
+      return res.status(500).json({ message: Messages.ERROR });
     }
   };
 
-  /**
-   * Creates a new diabetes type.
-   *
-   * @async
-   * @param {Object} req - The request object.
-   * @param {Object} res - The response object.
-   * @return {Promise<void>} - A promise that resolves to void.
-   * @throws {Error} - If an error occurs during the process.
-   */
   static addType = async (req, res) => {
     logger.info('Executing DiabetesTypeController.addType');
     
@@ -57,27 +40,16 @@ class DiabetesTypeController {
       const result = await DiabetesTypeDAO.add(newType);
 
       if (result.success) {
-        res
-          .status(201)
-          .json({ message: result.message, description: result.description });
+        return res.status(201).json(result.diabetes_type);
       } else {
-        res.status(500).json({ message: result.message });
+        return res.status(500).json({ message: result.message });
       }
     } catch (error) {
-      logger.error('Error DiabetesTypeController.addType');
-      res.status(500).json({ message: Messages.ERROR });
+      logger.error(`Error DiabetesTypeController.addType - Details: ${error}`);
+      return res.status(500).json({ message: Messages.ERROR });
     }
   };
 
-  /**
-   * Retrieves a diabetes type by its ID.
-   *
-   * @async
-   * @param {Object} req - The request object.
-   * @param {Object} res - The response object.
-   * @return {Promise<void>} - A promise that resolves to void.
-   * @throws {Error} - If an error occurs during the process.
-   */
   static getTypeById = async (req, res) => {
     logger.info('Executing DiabetesTypeController.getTypeById');
     try {
@@ -89,25 +61,16 @@ class DiabetesTypeController {
       const result = await DiabetesTypeDAO.getById(id);
 
       if (result.success) {
-        res.status(200).json(result.type);
+        return res.status(200).json(result.type);
       } else {
-        res.status(404).json({ message: result.message });
+        return res.status(404).json({ message: result.message });
       }
     } catch (error) {
-      logger.error('Error DiabetesTypeController.getTypeById');
-      res.status(500).json({ message: Messages.ERROR });
+      logger.error(`Error DiabetesTypeController.getTypeById - Details: ${error}`);
+      return res.status(500).json({ message: Messages.ERROR });
     }
   };
 
-  /**
-   * Updates a diabetes type by its ID.
-   *
-   * @async
-   * @param {Object} req - The request object.
-   * @param {Object} res - The response object.
-   * @return {Promise<void>} - A promise that resolves to void.
-   * @throws {Error} - If an error occurs during the process.
-   */
   static updateTypeById = async (req, res) => {
     logger.info('Executing DiabetesTypeController.updateTypeById');
     try {
@@ -125,25 +88,16 @@ class DiabetesTypeController {
       const result = await DiabetesTypeDAO.updateById(id, updatedType);
 
       if (result.success) {
-        res.status(200).json(result.type);
+        return res.status(200).json(result.type);
       } else {
-        res.status(404).json({ message: result.message });
+        return res.status(404).json({ message: result.message });
       }
     } catch (error) {
-      logger.error('Error DiabetesTypeController.updateTypeById');
-      res.status(500).json({ message: Messages.ERROR });
+      logger.error(`Error DiabetesTypeController.updateTypeById - Details: ${error}`);
+      return res.status(500).json({ message: Messages.ERROR });
     }
   };
 
-  /**
-   * Deletes a diabetes type by its ID.
-   *
-   * @async
-   * @param {Object} req - The request object.
-   * @param {Object} res - The response object.
-   * @return {Promise<void>} - A promise that resolves to void.
-   * @throws {Error} - If an error occurs during the process.
-   */
   static deleteTypeById = async (req, res) => {
     logger.info('Executing DiabetesTypeController.deleteTypeById');
     try {
@@ -155,13 +109,13 @@ class DiabetesTypeController {
       const result = await DiabetesTypeDAO.deleteById(id);
 
       if (result.success) {
-        res.status(200).json({ message: Messages.DIABETES_TYPE_DELETED });
+        return res.status(200).json({ message: Messages.DIABETES_TYPE_DELETED });
       } else {
-        res.status(404).json({ message: result.message });
+        return res.status(404).json({ message: result.message });
       }
     } catch (error) {
-      logger.error('Error DiabetesTypeController.deleteTypeById');
-      res.status(500).json({ message: Messages.ERROR });
+      logger.error(`Error DiabetesTypeController.deleteTypeById - Details: ${error}`);
+      return res.status(500).json({ message: Messages.ERROR });
     }
   };
 }
